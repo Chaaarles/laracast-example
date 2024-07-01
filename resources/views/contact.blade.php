77164702c354b337d124504dc5c1ea92ee551359
Petter Charles Redfern <petter.charles@redfern.no>
2024-07-01 19:48:00 +0200

    Initial commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Contact</x-slot:heading>
+    <h1>Wazzup! I'm Mr. Contact Page</h1>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Home</x-slot:heading>
+    <h1>Hello from Home</h1>
+</x-layout>
