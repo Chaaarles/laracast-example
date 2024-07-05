6d444fb23a3783414fa362525b224fbf3b4d4244
Petter Charles Redfern <petter.charles@redfern.no>
2024-07-05 14:03:31 +0200

    Episode 11 - Add model relationships

--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class JobListing extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'salary'];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
