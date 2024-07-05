a943ae7f9f4adbc6bd010118a0249963513cfb24
Petter Charles Redfern <petter.charles@redfern.no>
2024-07-05 12:59:35 +0200

    Episode 10 - Add employer and factories

---
+++ database/factories/JobListingFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employer;
+use App\Models\JobListing;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<JobListing>
+ */
+class JobListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => fake()->jobTitle(),
+            "description" => fake()->paragraph(),
+            "salary" => "600.000kr",
+            "employer_id" => Employer::factory(),
+        ];
+    }
+}
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,14 @@
 
     <div class="flex flex-row flex-wrap justify-evenly">
         @foreach($jobs as $job)
-            <article class="mb-6 bg-white shadow rounded-lg overflow-hidden w-72">
+            <article class="mb-6 bg-white shadow rounded-lg overflow-hidden w-72 flex flex-col">
                 <div class="px-6 py-4">
                     <h2 class="text-xl font-bold text-gray-800">{{ $job["title"] }}</h2>
-                    <p class="text-gray-600">{{ $job["description"] }}</p>
                     <p class="text-gray-600">{{ $job["salary"] }}</p>
+                    <p class="text-gray-600">{{ $job["description"] }}</p>
                 </div>
-                <div class="px-6 py-4 bg-gray-100">
+                <div class="grow"></div>
+                <div class="px-6 py-4 bg-teal-100">
                     <a href="/jobs/{{$job["id"]}}" class="text-indigo-600 hover:text-indigo-800">View Job</a>
                 </div>
             </article>
