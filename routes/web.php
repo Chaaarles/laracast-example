8f57fce78472e4512a73f9d38416ef0005ea6876
Petter Charles Stellberg Redfern <petter.charles@redfern.no>
2024-08-02 19:28:12 +0200

    Episode 24 - Working with queues

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,19 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\BingoJob;
+use App\Models\JobListing;
 use Illuminate\Support\Facades\Route;
+
+Route::get('test', function () {
+    dispatch(function () {
+        logger('Hello from the queue!');
+    });
+
+    BingoJob::dispatch(JobListing::first());
+
+    return 'Done';
+});
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
